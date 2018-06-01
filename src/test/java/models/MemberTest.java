28c037fa0c9d9af53f23b9d050cf84f8c97ca6b2
Garnett Puett <gpuett@gmail.com>
2018-06-01 13:07:14 -0700

    add member test

--- src/test/java/models/MemberTest.java
+++ src/test/java/models/MemberTest.java
@@ -6,7 +6,14 @@ import static org.junit.Assert.*;
 
 public class MemberTest {
     @Test
-    public void newMember_memberIstantiatesCorrectly() {
+    public void newMember_memberInstantiatesCorrectly() {
+        Member member = new Member("John");
+        assertEquals(true, member instanceof Member);
+    }
 
+    @Test
+    public void getName_getNamesCorrectly() {
+        Member member = new Member("John");
+        assertEquals("John", member.getName());
     }
 }
