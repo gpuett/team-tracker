933961e7e28a5682d50f77c3a6bd8f02658c4dda
Garnett Puett <gpuett@gmail.com>
2018-06-01 10:47:47 -0700

    add team form and detail pages

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,42 @@
+import models.Team;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "team-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Team> teams = Team.getAll();
+            model.put("teams", teams);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("name");
+            String description = request.queryParams("description");
+            Team newTeam = new Team(name, description);
+            String member1 = request.queryParams("member1");
+            newTeam.addMember(member1);
+            String member2 = request.queryParams("member2");
+            newTeam.addMember(member2);
+            String member3 = request.queryParams("member3");
+            newTeam.addMember(member3);
+            model.put("teams", newTeam);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
