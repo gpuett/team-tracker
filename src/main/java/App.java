5272fd9316904b8f493c76cb036ee05f4473d7cd
Garnett Puett <gpuett@gmail.com>
2018-06-08 10:25:57 -0700

    begin refactoring routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,55 +1,70 @@
-//import models.Member;
-//import models.Team;
-//import spark.ModelAndView;
-//import spark.template.handlebars.HandlebarsTemplateEngine;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import static spark.Spark.*;
-//
-//public class App {
-//    public static void main(String[] args) {
-//        staticFileLocation("/public");
-//
-//        get("/teams/new", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            return new ModelAndView(model, "team-form.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//        get("/", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            ArrayList<Team> teams = Team.getAll();
-//            model.put("teams", teams);
-//            return new ModelAndView(model, "index.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//        post("/teams/new", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            String name = request.queryParams("name");
-//            String description = request.queryParams("description");
-//            Team newTeam = new Team(name, description);
-//            String name1 = request.queryParams("member1");
-//            Member member1 = new Member(name1);
-//            newTeam.addMember(member1);
-//            String name2 = request.queryParams("member2");
-//            Member member2 = new Member(name2);
-//            newTeam.addMember(member2);
-//            String name3 = request.queryParams("member3");
-//            Member member3 = new Member(name3);
-//            newTeam.addMember(member3);
-//            model.put("teams", newTeam);
-//            return new ModelAndView(model, "success.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//        get("/teams/:id", (req, res) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            int idOfTeamToFind = Integer.parseInt(req.params("id"));
-//            Team foundTeam = Team.findById(idOfTeamToFind);
-//            model.put("team", foundTeam);
-//            return new ModelAndView(model, "team-detail.hbs");
-//        }, new HandlebarsTemplateEngine());
+import models.Member;
+import models.Team;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.Spark.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.sql2o.Sql2o;
+import dao.*;
+
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String connectionString = "jdbc:h2:~/todolist.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        Sql2oTeamDao teamDao = new Sql2oTeamDao(sql2o);
+        Sql2oMemberDao memberDao = new Sql2oMemberDao(sql2o);
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Team> teams = teamDao.getAll();
+            model.put("teams", teams);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "team-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        post("/teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("name");
+            String description = request.queryParams("description");
+            Team newTeam = new Team(name, description);
+            teamDao.add(newTeam);
+            int newTeamId = newTeam.getId();
+            String name1 = request.queryParams("member1");
+            Member member1 = new Member(name1, newTeamId);
+            memberDao.add(member1);
+            String name2 = request.queryParams("member2");
+            Member member2 = new Member(name2, newTeamId);
+            memberDao.add(member2);
+            String name3 = request.queryParams("member3");
+            Member member3 = new Member(name3, newTeamId);
+            memberDao.add(member3);
+            model.put("teams", newTeam);
+            response.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+        get("/teams/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfTeamToFind = Integer.parseInt(req.params("id"));
+            Team foundTeam = teamDao.findById(idOfTeamToFind);
+            model.put("team", foundTeam);
+            List<Member> allMembersByTeam = teamDao.getAllMembersByTeam(idOfTeamToFind);
+            model.put("members", allMembersByTeam);
+            return new ModelAndView(model, "team-detail.hbs");
+        }, new HandlebarsTemplateEngine());
 //
 //        post("/teams/:id", (req, res) -> {
 //            Map<String, Object> model = new HashMap<>();
@@ -92,5 +107,5 @@
 //            deleteTeam.deleteTeam();
 //            return new ModelAndView(model, "success.hbs");
 //        }, new HandlebarsTemplateEngine());
-//    }
-//}
+    }
+}
