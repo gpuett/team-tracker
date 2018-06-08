c10e43222fafa18065331982f6761235b7fe99d7
Garnett Puett <gpuett@gmail.com>
2018-06-08 09:13:37 -0700

    add teamDao test file

---
+++ src/test/java/dao/Sql2oTeamDaoTest.java
@@ -0,0 +1,55 @@
+package dao;
+
+import models.*;
+import org.junit.*;
+import org.sql2o.*;
+
+import static org.junit.Assert.*;
+
+public class Sql2oTeamDaoTest {
+    private Sql2oTeamDao teamDao;
+    private Sql2oMemberDao memberDao;
+    private Connection conn;
+
+    @Before
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        teamDao = new Sql2oTeamDao(sql2o);
+        conn = sql2o.open();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        conn.close();
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void deleteById() {
+    }
+
+    @Test
+    public void clearAllTeams() {
+    }
+
+    @Test
+    public void getAllMembersByTeam() {
+    }
+}
